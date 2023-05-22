c88463e3a242d509e87df62212bd3afb65380e12
Skye <25wells7183@daretolearn.org>
2023-05-22 11:50:02 -0400

    ahhh

---
+++ Pmain.py
@@ -0,0 +1,21 @@
+import pygame
+import config
+from game_state import Gamestate
+#Timestamp 1:12
+from game import Game
+pygame.init()
+FPS = 60
+screen = pygame.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
+
+pygame.display.set_caption("Pokemon Pygame")
+
+game = Game(screen)
+
+game.set_up()
+
+clock = pygame.time.Clock()
+while game.game_state == Gamestate.RUNNING:
+    clock.tick(60)
+    game.update()
+    pygame.display.flip()
+   
---
+++ config.py
@@ -0,0 +1,21 @@
+#https://www.rapidtables.com/web/color/RGB_Color.html#color-table
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+SILVER = (192, 192, 192)
+GRAY = (128, 128, 128)
+GREEN = (0,255,0)
+ORANGE = (255, 165, 0)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+CYAN = (0, 255, 255)
+MAGENTA = (255,0,255)
+PURPLE = (138,43,226)
+BRIGHT_PURPLE = (148,0,211)
+PINK = (255,105,180)
+HOT_PINK = (255,20,147)
+
+#------------------------------------------------------------
+SCALE = 32
+SCREEN_HEIGHT = 480
+SCREEN_WIDTH = 640
